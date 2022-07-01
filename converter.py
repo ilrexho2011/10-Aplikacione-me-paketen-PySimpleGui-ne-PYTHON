9e3e503b8a8f1da80588c571effd29207c85dc18
Ilirjan Rexho <61479363+ilrexho2011@users.noreply.github.com>
2022-07-01 19:23:40 +0200

    Update converter.py

--- converter.py
+++ converter.py
@@ -1,38 +1,37 @@
 import PySimpleGUI as sg
 
 layout = [
-	[
-		sg.Input(key = '-INPUT-'),
-		sg.Spin(['km to mile','kg to pound','sec to min'], key = '-UNITS-'), 
-		sg.Button('Convert', key = '-CONVERT-')
-	],
-	[sg.Text('Output', key = '-OUTPUT-')]
+    [
+        sg.Input(key = '-INPUT-'),
+        sg.Spin(['km ne milje', 'kg ne pound', 'sec ne min'], key = '-UNITS-'),
+        sg.Button('Konverto', key = '-CONVERT-')
+    ],
+    [sg.Text('Rezultati', key = '-OUTPUT-')]
 ]
-
-window = sg.Window('Converter',layout)
+window = sg.Window('Konvertuesi', layout)
 
 while True:
-	event, values = window.read()
+    event, values = window.read()
 
-	if event == sg.WIN_CLOSED:
-		break
+    if event == sg.WIN_CLOSED:
+        break
 
-	if event == '-CONVERT-':
-		input_value = values['-INPUT-']
-		if input_value.isnumeric():
-			match values['-UNITS-']:
-				case 'km to mile':
-					output = round(float(input_value) * 0.6214,2)
-					output_string = f'{input_value} km are {output} miles.'
-				case 'kg to pound':
-					output = round(float(input_value) * 2.20462,2)
-					output_string = f'{input_value} kg are {output} pounds.'
-				case 'sec to min':
-					output = round(float(input_value) / 60,2)
-					output_string = f'{input_value} seconds are {output} minutes.'
+    if event == '-CONVERT-':
+        input_value = values['-INPUT-']
+        if input_value.isnumeric():
+            match values['-UNITS-']:
+                case 'km ne milje':
+                    output = round(float(input_value) * 0.6214, 2)
+                    output_string = f'{input_value} Km jane {output} Milje'
+                case 'kg ne pound':
+                    output = round(float(input_value) * 2.20462, 2)
+                    output_string = f'{input_value} Kg jane {output} Pound'
+                case 'sec ne min':
+                    output = round(float(input_value) / 60, 2)
+                    output_string = f'{input_value} sec jane {output} Minuta'
 
-			window['-OUTPUT-'].update(output_string)
-		else:
-			window['-OUTPUT-'].update('Please enter a number')
+            window['-OUTPUT-'].update(output_string)
+        else:
+            window['-OUTPUT-'].update('Futni nje numer')
 
 window.close()
